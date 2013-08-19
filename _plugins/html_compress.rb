14f3ab609d9600234604f53bc85b726b3483c1a0
Ryan Seys <ryan@ryanseys.com>
2013-08-18 23:48:22 -0700

    Initial new style

---
+++ _plugins/html_compress.rb
@@ -0,0 +1,76 @@
+require 'html_press'
+
+module Jekyll
+  module Compressor
+    def exclude?(dest, dest_path)
+      res = false
+      file_name = dest_path.slice(dest.length+1..dest_path.length)
+      exclude = @site.config['html-compress'] && @site.config['html-compress']['exclude']
+      if exclude
+        if exclude.is_a? String
+          exclude = [exclude]
+        end
+        exclude.each do |e|
+          if e == file_name || File.fnmatch(e, file_name)
+            res = true
+            break
+          end
+        end
+      end
+      res
+    end
+
+    def output_file(dest, content)
+      FileUtils.mkdir_p(File.dirname(dest))
+      File.open(dest, 'w') do |f|
+        f.write(content)
+      end
+    end
+
+    def output_html(path, content)
+      output_file(path, HtmlPress.press(content, {}))
+    end
+  end
+
+  class Post
+    include Compressor
+
+    def write(dest)
+      dest_path = destination(dest)
+      output_html(dest_path, output)
+    end
+  end
+
+  class Page
+    include Compressor
+
+    def write(dest)
+      dest_path = destination(dest)
+      if exclude?(dest, dest_path)
+        output_file(dest_path, output)
+      else
+        output_html(dest_path, output)
+      end
+    end
+  end
+
+  class StaticFile
+    include Compressor
+
+    def copy_file(path, dest_path)
+      FileUtils.mkdir_p(File.dirname(dest_path))
+      FileUtils.cp(path, dest_path)
+    end
+
+    def write(dest)
+      dest_path = destination(dest)
+
+      return false if File.exist?(dest_path) and !modified?
+      @@mtimes[path] = mtime
+
+      copy_file(path, dest_path)
+
+      true
+    end
+  end
+end
--- js/ryanseys.js
+++ js/ryanseys.js
@@ -1,3 +1,19 @@
-function scroll_to(element) {
-  document.getElementById(element).scrollIntoView(true);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-39341257-1']);
+_gaq.push(['_setDomainName', 'ryanseys.com']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function toggleMenu() {
+  if(links.className == "show-phone") {
+    links.className = "hidden-phone";
+  }
+  else {
+    links.className = "show-phone";
+  }
 }
